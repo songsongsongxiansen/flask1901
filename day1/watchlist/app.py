ad30ac589e6c25619c342fe1068eadc656fd338a
song <1649996092@qq.com>
2020-04-07 17:36:09 +0800

    增加首页模板

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
-    return "<h1>hello,Flask 中国</h1>"
-
-@app.route('/index/<name>')
-def home(name):
-    return "<h1>Hello, %s</h1>"%name
+    name = "Bruce"
+    movies = [
+        {"title":"大赢家","year":"2020"},
+        {"title":"囧妈","year":"2020"},
+        {"title":"战狼","year":"2020"},
+        {"title":"心花路放","year":"2018"},
+        {"title":"速度与激情8","year":"2012"},
+        {"title":"我的父亲母亲","year":"1995"}
+    ]
+    return render_template('index.html',name=name,movies=movies)
