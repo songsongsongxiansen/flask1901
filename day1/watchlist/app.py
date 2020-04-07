cbec041d0ab9063288ecfd9d103bcb90568b2c7e
song <1649996092@qq.com>
2020-04-07 21:43:34 +0800

    用户验证

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -2,8 +2,10 @@ import os
 import sys
 
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash,check_password_hash
 import click
 from flask import Flask,render_template,flash,redirect,request
+from flask_login import LoginManager,UserMixin,login_user,logout_user,login_required
 
 WIN = sys.platform.startswith('win')
 if WIN:
@@ -19,16 +21,33 @@ app.config['SECRET_KEY'] = 'watchlist_dev'
 
 db = SQLAlchemy(app) #初始化
 
+login_manager = LoginManager(app) # 实例化登录拓展类
+
+@login_manager.user_loader
+def load_user(user_id):
+    user = User.query.get(int(user_id))
+    return user
+
+
+
 
 # models
-class User(db.Model):
+class User(db.Model,UserMixin):
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(20))
+    username = db.Column(db.String(20))
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self,password):
+        self.password_hash = generate_password_hash(password)
+    def validate_password(self,password):
+        return check_password_hash(self.password_hash,password)
+
 class Movie(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     title = db.Column(db.String(60))
     year = db.Column(db.String(4))
-
+    
 
 # 模板上下文处理函数
 @app.context_processor
@@ -78,6 +97,7 @@ def edit(movie_id):
 
 # 删除电影信息
 @app.route('/movie/delete/<int:movie_id>',methods=['POST'])
+@login_required
 def delete(movie_id):
     movie = Movie.query.get_or_404(movie_id)
     db.session.delete(movie)
@@ -85,7 +105,38 @@ def delete(movie_id):
     flash('删除成功')
     return redirect(url_for('index'))
     
-    
+#登录
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        # request在请求触发的时候才会包含数据
+        username = request.form['username']
+        password = request.form['password']
+        # 验证数据
+        if not username or not password:
+            flash('输入错误')
+            return redirect(url_for('login'))
+        # 和数据库中的信息就行比对验证
+        user = User.query.first()
+        if user.username == username and user.validate_password(password):
+            login_user(user)
+            flash('登录成功')
+            return redirect(url_for('index'))
+        flash('用户名或密码错误')
+        return redirect(url_for('login'))
+    return render_template('login.html')
+
+# 登出
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('拜拜')
+    return redirect(url_for('index'))
+
+
+
+
     
 # 自定义命令
 @app.cli.command()  # 装饰器，注册命令
@@ -121,6 +172,24 @@ def forge():
         db.session.add(movie)
     db.session.commit()
     click.echo("插入数据完成")
+
+@app.cli.command()
+@click.option('--username',prompt=True,help='管理员账号')
+@click.option('--password',prompt=True,help='管理员密码',hide_input=True,confirmation_prompt=True)
+def admin(username,password):
+    user = User.query.first()
+    if user is not None:
+        click.echo('更新用户信息')
+        user.username = username
+        user.set_password(password)
+    else:
+        click.echo('创建用户信息')
+        user = User(username=username,name='Admin')
+        user.set_password(password)
+        db.session.add(user)
+    db.session.commit()
+    click.echo('管理员创建完成')
+
 
 @app.errorhandler(404)
 def page_not_found(e):
