0445d66efb615c10c537095365c6c343484b30e1
song <1649996092@qq.com>
2020-04-07 16:17:25 +0800

    第一个Flask项目和环境变量

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -1,5 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
+
 @app.route('/')
+
 def index():
-    return "<h1>hello,Flask</h1>"
+    return "<h1>hello,Flask 中国</h1>"
+
+@app.route('/index/<name>')
+def home(name):
+    return "<h1>Hello, %s</h1>"%name
