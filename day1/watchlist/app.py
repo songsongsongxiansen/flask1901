9891f1e4682adf14415a6e2ec3652821c6357dc8
song <1649996092@qq.com>
2020-04-07 19:04:44 +0800

    数据库操作

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -1,7 +1,35 @@
-from flask import Flask
+import os
+import sys
+
+from flask_sqlalchemy import SQLAlchemy
+import click
 from flask import Flask,render_template
+
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path,'data.db')
+app.config['SQLALCHY_TRACK_MODIFICATIONS'] = False # 关闭了对模型修改的监控
+
+db = SQLAlchemy(app) #初始化
+
+
+# models
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(20))
+class Movie(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+
+# views
 @app.route('/')
 def index():
     name = "Bruce"
@@ -20,3 +48,13 @@ def index():
         {"title":"我的父亲母亲","year":"1995"}
     ]
     return render_template('index.html',name=name,movies=movies)
+
+
+# 自定义命令
+@app.cli.command()  # 装饰器，注册命令
+@click.option('--drop',is_flag=True,help="删除后再创建")
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo("初始化数据库完成") 
