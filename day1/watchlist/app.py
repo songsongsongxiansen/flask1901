38bc637bfb5955d5d59113e96602268b5fdc65fd
song <1649996092@qq.com>
2020-04-07 17:59:10 +0800

    静态资源

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -11,6 +11,12 @@ def index():
         {"title":"战狼","year":"2020"},
         {"title":"心花路放","year":"2018"},
         {"title":"速度与激情8","year":"2012"},
+        {"title":"我的父亲母亲","year":"1995"},
+        {"title":"大赢家","year":"2020"},
+        {"title":"囧妈","year":"2020"},
+        {"title":"战狼","year":"2020"},
+        {"title":"心花路放","year":"2018"},
+        {"title":"速度与激情8","year":"2012"},
         {"title":"我的父亲母亲","year":"1995"}
     ]
     return render_template('index.html',name=name,movies=movies)
